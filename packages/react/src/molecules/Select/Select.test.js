a8d8f0cf1c860f89589f791e2c0114415c48fc9d
Vanessa Bautista <vanessawebdev@gmail.com>
2023-04-01 05:03:21 -0700

    added tests

--- packages/react/src/molecules/Select/Select.test.js
+++ packages/react/src/molecules/Select/Select.test.js
@@ -25,3 +25,75 @@ test("renders all options passed to it", () => {
 
   expect(getAllByRole("menuitemradio")).toHaveLength(options.length);
 });
+
+test("the button label changes to the selected option label", () => {
+  const { getAllByRole, getByTestId } = render(<Select options={options} />);
+
+  fireEvent.click(getByTestId("DseSelectButton"));
+  fireEvent.click(getAllByRole("menuitemradio")[0]);
+
+  expect(getByTestId("DseSelectButton")).toHaveTextContent(options[0].label);
+});
+
+test("snapshop of the selected option state", () => {
+  const { getAllByRole, getByTestId, asFragment } = render(
+    <Select options={options} />
+  );
+
+  fireEvent.click(getByTestId("DseSelectButton"));
+  fireEvent.click(getAllByRole("menuitemradio")[0]);
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("snapshot of the base state", () => {
+  const { asFragment } = render(<Select options={options} />);
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("can customize select label", () => {
+  const { getByText } = render(
+    <Select options={options} label="CUSTOM LABEL" />
+  );
+
+  expect(getByText(/CUSTOM LABEL/)).toBeInTheDocument();
+});
+
+// test("renders options using custom renderOption method if passed as prop", () => {
+//   const { getAllByRole, getByTestId, getAllByTestId } = render(
+//     <Select
+//       options={options}
+//       renderOptions={({ option, getRecommendedProps }) => {
+//         return (
+//           <li data-testid="CustomRenderOption" {...getRecommendedProps()}>
+//             {option.label}
+//           </li>
+//         );
+//       }}
+//     />
+//   );
+//   fireEvent.click(getByTestId("DseSelectButton"));
+
+//   expect(getAllByTestId("CustomRenderOption")).toHaveLength(options.length);
+// });
+
+// test("calls the onOptionSelected prop with the selected option and its index if passed", () => {
+//   const onOptionSelected = jest.fn();
+//   const { getAllByRole, getByTestId } = render(
+//     <Select options={options} onOptionSelected={onOptionSelected} />
+//   );
+
+//   fireEvent.click(getByTestId("DseSelectButton"));
+//   fireEvent.click(getAllByRole("menuitemradio")[0]);
+
+//   expect(onOptionSelected).toHaveBeenCalledWith(options[0], 0)
+// });
+
+// test("snapshop of the options menu opten nstate", () => {
+//   const { getByTestId } = render(<Select options={options} />);
+
+//   fireEvent.click(getByTestId("DseSelectButton"));
+
+//   expect(getByTestId("DseSelectButton")).toHaveTextContent(options[0].label);
+// });
