1bb6786a6e0e1c8a9d40b90e5a38ee0719235f93
Vanessa Bautista <vanessawebdev@gmail.com>
2023-04-02 11:31:29 -0700

    added story to tabs in story book

--- packages/react/src/molecules/Select/Select.stories.tsx
+++ packages/react/src/molecules/Select/Select.stories.tsx
@@ -19,10 +19,28 @@ const options = [
   },
 ];
 
+// Group components by adding to the title
 export default {
-  title: "Select",
+  title: "Molecules | Select",
+  component: Select,
 };
 
 export const Common = () => <Select options={options} />;
 
-export const Common1 = () => <Select options={[]} />;
+export const CustomLabel = () => (
+  <Select options={options} label="Select a Color" />
+);
+
+// Provide Developer-helpful variants, look at the component interface to reflect variants
+
+// export const RenderOption = () => (
+//   <Select
+//     options={options}
+//     renderOption={({ getOptionRecommendedProps, option, isSelected }) => {
+//       <span {...getOptionRecommendedProps}>
+//         {option.label}
+//         {isSelected ? "SELECTED!" : ""}
+//       </span>;
+//     }}
+//   />
+// );
